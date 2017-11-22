31a7241ff854e94ca0686746daa493848079d4de
Justin Lemke <epicspyninja@gmail.com>
2017-11-21 20:04:12 -0800

    i m p l e m e n t s

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encodedMotor.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by robotics on 11/21/2017.
+ */
+
+public abstract class encodedMotor implements DcMotor{
+    public boolean isEncoded;
+}
